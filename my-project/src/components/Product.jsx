e251889489bdba9717c0250ed70485b246f36084
solomiiaprsk <solomiia.pruska@gmail.com>
2024-06-19 20:53:08 -0600

    3

---
+++ my-project/src/components/Product.jsx
@@ -0,0 +1,16 @@
+// src/components/Product.jsx
+
+export const Product = ({ name, imgUrl, price }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+			<img src={imgUrl} alt={name} width="480" />
+			<p>Price: {price} credits</p>
+    </div>
+  );
+};
+
+
+
+
+export default Product;
